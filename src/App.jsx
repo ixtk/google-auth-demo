1c8800ee500a44336e08827383d3fc7a07dedd4c
ixtk <ikhcamot@gmail.com>
2026-05-16 08:55:54 +0400

    Add simple logout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import './App.css'
 import {
   signInWithPopup,
-  onAuthStateChanged
+  onAuthStateChanged,
+  signOut
 } from 'firebase/auth'
 import { auth, googleProvider } from './firebase'
 import { useEffect, useState } from 'react'
@@ -15,8 +16,13 @@ function App() {
     console.log(result)
   }
 
+  async function handleGoogleLogut () {
+    await signOut(auth)
+  }
+
   useEffect(function() {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      console.log(currentUser)
       setUser(currentUser)
     })
     return unsubscribe
@@ -25,18 +31,24 @@ function App() {
   return (
     <div className="flex min-h-screen items-center justify-center p-4">
       <div className="flex w-full max-w-[320px] flex-col items-center gap-6 rounded-lg border p-8 shadow-sm">
+        {/* user !== null */}
         {user && (
           <div className="space-y-1 text-center">
+            <h1>{user.displayName}</h1>
+            <img src={user.photoURL} alt="" />
             <p className="text-muted-foreground">{user.email}</p>
           </div>
         )}
         
-        <Button 
-          className="w-full" 
-          onClick={handleGoogleLogin}
-        >
-          Login with Google
-        </Button>
+        {user ? (
+          <Button onClick={handleGoogleLogut} variant="secondary">
+            Logout
+          </Button>
+        ) : (
+          <Button className="w-full" onClick={handleGoogleLogin}>
+            Login with Google
+          </Button>
+        )}
       </div>
     </div>
   )
